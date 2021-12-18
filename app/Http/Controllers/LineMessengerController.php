5d51890402b993b150330a1ee63a7b11d4ae5159
Yukiman-byde <yukiman8827@icloud.com>
2021-12-18 06:29:14 +0000

    edit controller

--- app/Http/Controllers/LineMessengerController.php
+++ app/Http/Controllers/LineMessengerController.php
@@ -11,20 +11,9 @@ class LineMessengerController extends Controller
 {
     public function webhook(Request $request) {
         // LINEから送られた内容を$inputsに代入
-        return "200 ok";
         $inputs=$request->all();
-
-        // そこからtypeをとりだし、$message_typeに代入
-        $message_type=$inputs['events'][0]['type'];
-
-        // メッセージが送られた場合、$message_typeは'message'となる。その場合処理実行。
-        if($message_type=='message') {
-            
-            // replyTokenを取得
-            $reply_token=$inputs['events'][0]['replyToken'];
-
-            // LINEBOTSDKの設定
-            $http_client = new CurlHTTPClient(config('services.line.channel_token'));
+        $reply_token=$inputs['events'][0]['replyToken'];
+        $http_client = new CurlHTTPClient(config('services.line.channel_token'));
             $bot = new LINEBot($http_client, ['channelSecret' => config('services.line.messenger_secret')]);
 
             // 送信するメッセージの設定
@@ -34,6 +23,6 @@ class LineMessengerController extends Controller
             $reply=$bot->replyText($reply_token, $reply_message);
             
             return 'ok';
-        }
+
     }
 }
