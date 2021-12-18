b26c26a2ccdbeef6cceeeb31905ef9e898443ce7
Yukiman-byde <yukiman8827@icloud.com>
2021-12-18 17:27:34 +0000

    edit controller

--- app/Http/Controllers/LineMessengerController.php
+++ app/Http/Controllers/LineMessengerController.php
@@ -10,16 +10,15 @@ use Illuminate\Http\Request;
 class LineMessengerController extends Controller
 {
     public function webhook(Request $request) {
-        // LINEから送られた内容を$inputsに代入
-        // $reply_token = $request['events']['replyToken'];
-        // $channel_token = config('services.line.channel_token');
-        // $line_id = config('services.line.line_user');
-        // $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($channel_token);
-        // $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => env('LINE_MESSENGER_SECRET')]);
-        // $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('あれ？');
-        // $response=$bot->replyMessage($reply_token, $textMessageBuilder);
-        //echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
-       return $request['events'];
+        //LINEから送られた内容を$inputsに代入
+        $reply_token = $request['events'][0]['replyToken'];
+        $channel_token = config('services.line.channel_token');
+        $line_id = config('services.line.line_user');
+        $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($channel_token);
+        $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => env('LINE_MESSENGER_SECRET')]);
+        $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('あれ？');
+        $response=$bot->replyMessage($reply_token, $textMessageBuilder);
+        echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
     }
     
     public function message(){
