af4cc05dfd9770718b587219f146f431ea2d24e5
Yukiman-byde <yukiman8827@icloud.com>
2021-12-19 09:55:56 +0000

    edit controller

--- app/Http/Controllers/LineMessengerController.php
+++ app/Http/Controllers/LineMessengerController.php
@@ -35,6 +35,10 @@ class LineMessengerController extends Controller
         function replyTextMessage($bot, $replyToken, $text){
             $message = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text);
             $response = $bot->replyMessage($replyToken,$message);
+            
+            if(!$response->isSucceeded()){
+                error_log($response->getHTTPStatus. ' ' . $response->getRawBody);
+            }
         }
        // echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
     }
