88e9b6d3fdca7d5f561fef3c12310e13360fdd30
Yukiman-byde <yukiman8827@icloud.com>
2021-12-19 05:02:58 +0000

    edit controller

--- app/Http/Controllers/LineMessengerController.php
+++ app/Http/Controllers/LineMessengerController.php
@@ -5,35 +5,27 @@ use LINE\LINEBot;
 use App\Models\User;
 use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 use LINE\LINEBot\Constant\HTTPHeader;
-
+use LINE\LINEBot\SignatureValidator;
 use Illuminate\Http\Request;
 
 class LineMessengerController extends Controller
 {
     public function webhook(Request $request) {
-        //LINEから送られた内容を$inputsに代入
-        $signature = $request->headers->get(HTTPHeader::LINE_SIGNATURE);
-        
         $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(config('services.line.channel_token'));
         $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => env('LINE_MESSENGER_SECRET')]);
-     
-        $events = $bot->parseEventRequest($request->getContent(), $signature);
-        
-        foreach($events as $event){
-            $replyToken = $event->getReplyToken();
-            $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('あれ？');
-            $response=$bot->replyMessage('https://linetestapiwebhook.free.beeceptor.com', $textMessageBuilder);
-        }
-        
+        $replyToken = $request->getReplyToken();
+        $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('あれ？');
+        $response=$bot->replyMessage($replyToken, $textMessageBuilder);
         echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
     }
     
     public function message(){
         $line_id = config('services.line.line_user');
         $channel_token = config('services.line.channel_token');
+     
         $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($channel_token);
         $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => env('LINE_MESSENGER_SECRET')]);
-       // dd($bot);
+        //dd($bot);
        // $textMessageBuilder = new TextMessageBuilder('ヤッホー');
         $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('ehe？');
         $response = $bot->pushMessage($line_id, $textMessageBuilder);
