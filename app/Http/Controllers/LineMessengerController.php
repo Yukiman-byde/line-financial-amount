32816b5f504115afed3ec99c7f8d49104613f511
Yukiman-byde <yukiman8827@icloud.com>
2021-12-21 04:19:15 +0000

    edit controller

--- app/Http/Controllers/LineMessengerController.php
+++ app/Http/Controllers/LineMessengerController.php
@@ -36,7 +36,7 @@ class LineMessengerController extends Controller
 
         foreach($events as $event){
              $group_id = $event->getGroupId();
-             $res = $group_id->getGroupSummary();
+             $res = $bot->$group_id->getGroupSummary();
             // $data = $res->getJSONDecodedBody();
             // $this->assertEquals('Group name', $data['groupName']);
             //$data = $res->getJSONDecodedBody();
