2553c88395da028d19fe0d2268ebf6d3952c3537
Yukiman-byde <yukiman8827@icloud.com>
2021-12-21 05:06:03 +0000

    edit controller

--- app/Group.php
+++ app/Group.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Group extends Model
 {
-    //
+     protected $fillable = [
+       'name', 'groupID', 'pictureUrl',
+    ];
+    
+    protected $hidden = [
+        'groupID',
+    ];
 }
