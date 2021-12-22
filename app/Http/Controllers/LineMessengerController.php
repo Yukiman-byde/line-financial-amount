6e280d7e126f7418ed7bebeca7758964f06222fe
Yukiman-byde <yukiman8827@icloud.com>
2021-12-22 17:52:33 +0000

    edit controller

--- app/Http/Controllers/LineMessengerController.php
+++ app/Http/Controllers/LineMessengerController.php
@@ -86,7 +86,8 @@ class LineMessengerController extends Controller
            
     public function groupstore($bot, $replyToken, $event){
        $group_id = $event->getGroupId();
-       $response = $bot->replyMessage($replyToken, $group_id);
+       $message = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($group_id);
+       $response = $bot->replyMessage($replyToken, $message);
        return '200';
        $res = $bot->getGroupSummary($group_id);
        $data = $res->getJSONDecodedBody();
