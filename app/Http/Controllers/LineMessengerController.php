af29d230abff3d3ce9031e949887aef8bd03fc9b
Yukiman-byde <yukiman8827@icloud.com>
2021-12-23 06:39:52 +0000

    edit controller

--- app/Http/Controllers/LineMessengerController.php
+++ app/Http/Controllers/LineMessengerController.php
@@ -90,7 +90,7 @@ class LineMessengerController extends Controller
     //   $res = $bot->getGroupSummary($group_id);
     //   $data = $res->getJSONDecodedBody();
     //   $name = $data['groupName'];
-       //$message = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($group_id);
+       $message = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($group_id);
        $response = $bot->replyMessage($replyToken, $group_id);
        return '200';
        $res = $bot->getGroupSummary($group_id);
