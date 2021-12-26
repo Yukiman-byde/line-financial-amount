68d80d496522a231e3989c4162c6459d39959c0c
Yukiman-byde <yukiman8827@icloud.com>
2021-12-26 14:01:48 +0000

    edit controllers

--- app/Http/Controllers/LineMessengerController.php
+++ app/Http/Controllers/LineMessengerController.php
@@ -53,7 +53,7 @@ class LineMessengerController extends Controller
                    $response = $this->replyTextMessage($bot, $event->getReplyToken(), 'こちらが結果になります');
                    break;
                    
-               case $event['mention']['mentionees']['userId']:
+               case $event['mentionees']['userId']:
                    $response = $this->replyTextMessage($bot, $event->getReplyToken(), '成功したよ〜ん');
                    break;
                    
