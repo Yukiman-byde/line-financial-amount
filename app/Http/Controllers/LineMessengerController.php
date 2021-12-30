d6d4c392c00787a149f9e84f70e92be35e561543
Yukiman-byde <yukiman8827@icloud.com>
2021-12-30 15:44:10 +0000

    edit controller

--- app/Http/Controllers/LineMessengerController.php
+++ app/Http/Controllers/LineMessengerController.php
@@ -55,7 +55,7 @@ class LineMessengerController extends Controller
                case '結果を見る':
                    $response = $this->replyTextMessage($bot, $event->getReplyToken(), 'こちらが結果になります');
                    break;
-               case User::where('provided_user_id', $event->getText()) !== null:
+               case User::where('provided_user_id', $event->getPostBackData()) !== null:
                    $response = $this->replyTextMessage($bot, $event->getReplyToken(), 'います');
                 //   $this->retrive_chars($event);
                 //   $amount = new Amount;
