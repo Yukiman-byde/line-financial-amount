ad7c72c01625738b511ab25bec006fa87759a49b
Yukiman-byde <yukiman8827@icloud.com>
2021-12-31 13:49:59 +0000

    edit controller

--- app/Http/Controllers/LineMessengerController.php
+++ app/Http/Controllers/LineMessengerController.php
@@ -61,7 +61,7 @@ class LineMessengerController extends Controller
                    break;
              }
         
-          switch($event->getPostbackData()){
+          switch(strval($event->getPostbackData())){
               case $event->getPostbackData():
                   $response = $this->replyTextMessage($bot, $event->getReplyToken(), '成功しました');
                  break;
