d84023c95a38f6bc2d7d3c2c6d609b4c748a48b7
Yukiman-byde <yukiman8827@icloud.com>
2022-01-07 08:06:52 +0000

    edit LaravelApi

--- resources/js/components/LaravelApi.js
+++ resources/js/components/LaravelApi.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
  export default axios.create({
         baseURL: 'https://amount-money.herokuapp.com/',
-    });
+    }); 
  // export default axios.create({
  //        baseURL: 'https://0adeb2dd125c4667904a644ca5ae92f8.vfs.cloud9.ap-northeast-1.amazonaws.com/',
  //    });
